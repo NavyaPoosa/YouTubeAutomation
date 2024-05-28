d413d885c21ae1a7338faf720342120625dc2eb1
Rakesh Gundu <poosanavya1@gmail.com>
2024-05-28 08:46:05 +0530

    testcase02 done

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -1,9 +1,7 @@
 package demo;
 
 import java.time.Duration;
-
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -13,7 +11,7 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class TestCases {
-    ChromeDriver driver;
+    WebDriver driver;
 
    @BeforeClass
     public void setup() {
@@ -43,14 +41,41 @@ public class TestCases {
 
         //print the text dispalyed on the About page 
         WebElement textDisplay = SeleniumWrapper.findElement_Youtube(By.xpath("//section[@class='ytabout__content']"), driver);
-        SeleniumWrapper.javaScroll(textDisplay, driver);
+        SeleniumWrapper.javaScroll(textDisplay,driver);
+        Thread.sleep(2000);
         System.out.println(textDisplay.getText());
+        
 
         System.out.println("End Test case: Testcase01");
     }
+    
+    @Test
+    public void testcase02() throws InterruptedException{
+        System.out.println("Start Test case: Testcase02");
+        driver.navigate().back();
+        Thread.sleep(2000);
+        WebElement filmButton = SeleniumWrapper.findElement_Youtube(By.xpath("//yt-formatted-string[text()='Movies']"), driver);
+        Boolean testflow01 = SeleniumWrapper.click_Youtube(filmButton, driver);
+        Assert.assertEquals(testflow01, true);
 
-
-    @AfterClass
+        WebElement nextButton = SeleniumWrapper.findElement_Youtube(By.xpath("//button[@aria-label='Next']//div[@class='yt-spec-touch-feedback-shape__fill']"),driver);
+    
+        
+            for(int i=0;i<3;i++){
+                if(nextButton.isEnabled()){
+                    nextButton.click();
+                    Thread.sleep(1000);
+              }  
+            }
+        
+        WebElement movie = SeleniumWrapper.findElement_Youtube(By.xpath("//span[@title='The Wolf of Wall Street']/ancestor::a/span"), driver);
+       Thread.sleep(1000);
+        Boolean testflow02 = movie.getText().contains("Comedy");
+       Assert.assertEquals(testflow02, true);
+       System.out.println("End Test case: Testcase02");
+    }
+   
+     @AfterClass
     public void endTest() {
         System.out.println("End Test: TestCases");
         driver.close();
